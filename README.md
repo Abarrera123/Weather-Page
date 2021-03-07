49342747fefc0f502419ef1adae9134bb464b04f
Antonio Barrera <Barrera.antonio@gmail.com>
2021-03-07 09:35:07 -0800

    wwooo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Weather-Page
+
+Why: This website was created to help understand the use of server side APIs. This assignment also helped reinforce js,html,and css skills.
+What. This website allows the user to enter a city and hit search. After the button is hit the user is shown the current days weather along with a five day forecast. Below the search bar is a list of featured cities. If the user decides to hit any of the featured cities then the weather for that city will be shown.
+
+How:
+1. The first step for this website was to create a skeleton of the HTML. Using Bootstrap i added a nav bar, input field, and a search button. I will add the featured cities after i write the logic in js. 
+2. My next step was to declare my global variables in my js file. I set an event listerner to click on the search button. This button will call the inputSubmitCity function. This function will pull the value from the input field. We will then call the getCityWeather using the city as a paramenter for the function. 
+3. The getCityWeather function will use the paramenter of city and put the name of the city into a apiURL. This Url will give us the data we need for the current day's weather. Next the fetch api is used to return a json response. with that response we can then extract data and put them in our HTML elements. To get our five day weather and UV index another API is needed. To call this API I set the variable lat and long to the value in the data. 
+4. For the next API I enter the values of lat and long into the api url. Once we json the response of the fetch then I can extract the data. I create a for loop to create a 5 day forecast. In that loop I pull the temp, humidity, and wind speed for the day. 
+5. The last step I took was to repeat steps 3 and 4 but for the featured cities.  
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//var featuredCities = ['New York', 'Miami', 'Seatlle','Los Angeles', 'San Francisco', 'Miami', 'Austin', 'Las Vegas']
+
 var userInputEl = document.getElementById('user-input')
 var searchBtn = document.getElementById('button-addon2')
 var weatherContainerEl = document.getElementById('search-result');
@@ -10,13 +10,13 @@ var cityButton = document.getElementsByClassName('col-6 col-sm-3')
 var fiveDayContain = $('#five-day')
 var featured = $('#featured-container')
 
-
+//Function to get user input and call on the getCityWeather function
 var inputSubmitCity = function(event){
     event.preventDefault();
-
+    //extracts user input into variable city
     var city = userInputEl.value.trim();
     weatherContainerEl.textContent ="Showing Weather for: " + city 
-
+    //checking that the user has an input 
     if(city){
         getCityWeather(city);
         //reset the container of information
@@ -26,19 +26,27 @@ var inputSubmitCity = function(event){
 };
 
 var getCityWeather = function(city){
+    //using the user input to reaplce the city name 
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+ '&units=imperial&appid=cdc889a6c131e078f10ee5db1d9fa2e8';
-
+    //clearing all HTML elements
+    temp.textContent='Temp: ' 
+    humidity.textContent = 'Humidity: '
+    wind.textContent ='Wind Speed: '
+    uv.textContent='UV Index:'
     fetch(apiUrl)
     .then(function (response){
+        //making the data a usable string
         return response.json();
     })
     .then(function (data){
+            //using our data we are filling in the temp, humidity, wind speed, and weather img  of the current day.
             temp.textContent='Temp: ' +(data.main.temp)+ '\u00B0 Fahrenheit';
             humidity.textContent = 'Humidity: ' +data.main.humidity+'%';
             wind.textContent ='Wind Speed: ' +data.wind.speed+ 'MPH';
             var img = document.createElement('img');
             img.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
             weatherContainerEl.appendChild(img);
+            //setting lat and long for the next api
             var lat = data.coord.lat
             var long = data.coord.lon
             var uvApi= 'https://api.openweathermap.org/data/2.5/onecall?lat=' +lat+ '&lon='+long+'&exclude=hourly,minutely&units=imperial&appid=cdc889a6c131e078f10ee5db1d9fa2e8';
@@ -47,13 +55,16 @@ var getCityWeather = function(city){
                 return response.json();
             })
             .then(function (uvData){
+                //setting the UV index for the current day's weather
                 uv.textContent='UV Index:'+ uvData.current.uvi;
+                //for loop to make a 5 day forecast
                 for(i=1; i<6; i++){
+                //these four lines create the date 
                 var unixTime = parseInt(uvData.daily[i].dt)
                 var milliseconds = unixTime *1000
                 var dateObject = new Date(milliseconds)
                 var humanDateFormat = dateObject.toLocaleString() 
-
+               //These lines will generate HTML objects with all the data asked for. 
                var fiveDayHTMLF = '<div id = "day'+i+'">';
                 fiveDayHTMLF +='    <div id = "date'+i+'"> Date: ' +humanDateFormat+'</div>';
                 fiveDayHTMLF +='    <img src = "http://openweathermap.org/img/w/' + uvData.daily[i].weather[0].icon +'.png">';
@@ -70,20 +81,59 @@ var getCityWeather = function(city){
     })
 }
 
-var getUVindex= function(city){
 
-}
- //var featCitySearch = function(event){
-//     event.preventDefault();
-//     var featuredCity = cityButton.value;
-//     console.log(featuredCity);
-// }
-
-
-searchBtn.addEventListener('click', inputSubmitCity);
 featured.on('click', 'button',function(event){
+    //getting city name 
     event.preventDefault();
     var city = $(this).text();
-    console.log(city)
+    //The same exact code from above/
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+ '&units=imperial&appid=cdc889a6c131e078f10ee5db1d9fa2e8';
+    temp.textContent='Temp: ' 
+    humidity.textContent = 'Humidity: '
+    wind.textContent ='Wind Speed: '
+    uv.textContent='UV Index:'
+    fetch(apiUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+            weatherContainerEl.textContent ="Showing Weather for: " + city 
+            temp.textContent='Temp: ' +(data.main.temp)+ '\u00B0 Fahrenheit';
+            humidity.textContent = 'Humidity: ' +data.main.humidity+'%';
+            wind.textContent ='Wind Speed: ' +data.wind.speed+ 'MPH';
+            var img = document.createElement('img');
+            img.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+            weatherContainerEl.appendChild(img);
+            var lat = data.coord.lat
+            var long = data.coord.lon
+            var uvApi= 'https://api.openweathermap.org/data/2.5/onecall?lat=' +lat+ '&lon='+long+'&exclude=hourly,minutely&units=imperial&appid=cdc889a6c131e078f10ee5db1d9fa2e8';
+            fetch(uvApi)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function (uvData){
+                uv.textContent='UV Index:'+ uvData.current.uvi;
+                fiveDayContain.empty();
+                for(i=1; i<6; i++){
+                var unixTime = parseInt(uvData.daily[i].dt)
+                var milliseconds = unixTime *1000
+                var dateObject = new Date(milliseconds)
+                var humanDateFormat = dateObject.toLocaleString() 
+               
+               var fiveDayHTMLF = '<div id = "day'+i+'">';
+                fiveDayHTMLF +='    <div id = "date'+i+'"> Date: ' +humanDateFormat+'</div>';
+                fiveDayHTMLF +='    <img src = "http://openweathermap.org/img/w/' + uvData.daily[i].weather[0].icon +'.png">';
+                fiveDayHTMLF +='    <div id ="temp'+i+'"> Tempeture:'+uvData.daily[i].temp.day+ '\u00B0 Fahrenheit </div>';
+                fiveDayHTMLF +='    <div id ="humidity'+i+'"> Humidity:'+uvData.daily[i].humidity+ '% </div>';
+                fiveDayHTMLF += '   <div id = "wind-speed'+i+'">Wind-Speed:' +uvData.daily[i].wind_speed+ 'MPH</div>';
+                fiveDayHTMLF +='</div>'
+                fiveDayContain.append(fiveDayHTMLF);
+            }
+                console.log(humanDateFormat);
+            })
+            
+        
+    })
 })
   
+searchBtn.addEventListener('click', inputSubmitCity);
